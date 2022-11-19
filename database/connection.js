834aafe141ec12137be9923ff0c9cc58a7bc77eb
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-11-19 15:31:19 +0200

    Added postgres database setup

---
+++ database/connection.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
+})
+
+
+module.exports = pool
